b56c831a101d52b9b09d59008b91acd5b9f05f22
Diogo Rodrigues <diogoheck6@gmail.com>
2026-02-03 14:00:24 -0300

    fix: adjust migrations dir

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -18,7 +18,7 @@ export default async function migrations(request, response) {
     const defaultMigrationsOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: resolve("infra", "migrations"),
+      dir: resolve(process.cwd(), "infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
