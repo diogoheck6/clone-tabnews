d2a1e5edb1f857e38863d3a774ef579831287e74
Diogo Rodrigues <diogoheck6@gmail.com>
2026-06-25 11:33:33 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <h1>Ari, você é uma pessoa muito especial. Nunca se esqueça disso! 🌷</h1>
+    <h1>Ari, você é uma pessoa muito especial. Nunca se esqueça disso! 🌷❤️</h1>
   );
 }
 
